f6f7d17b8146197753047cd8efb71310a24bbde5
andreza <andrezabarbosamaria@hotmail.com>
2020-03-18 23:09:38 -0300

    Versão final.

--- app/src/main/java/br/com/bossini/fatec_ipi_noite_pdm_chat_com_firebase/MainActivity.java
+++ app/src/main/java/br/com/bossini/fatec_ipi_noite_pdm_chat_com_firebase/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Versão final", Toast.LENGTH_SHORT).show();
     }
 
     public void irParaCadastro (View v){
